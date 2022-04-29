36bd7961a39f7bb6b7fdfda37aa7f41d5f8dc72d
gonzaloccama <atil.menes.xd@gmail.com>
2022-04-29 14:08:48 -0500

    investment

---
+++ resources/views/livewire/admin/bonus-payment/index.blade.php
@@ -0,0 +1,6 @@
+<div class="col-md-12">
+    @include('livewire.widgets.admin.header.more-options')
+    <div class="separator mb-5"></div>
+
+    @include('livewire.widgets.admin.table.table-payment')
+</div>
